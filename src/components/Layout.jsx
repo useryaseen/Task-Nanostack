2f7da817a1036f4eb6f9c7a30df0a4c7c9b34307
User Yaseen <mohdyaseen7610@gmail.com>
2025-07-28 20:06:01 +0530

    changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,7 @@
+
+
 import { Outlet } from "react-router-dom";
-import Sidebar from "./SideBar"; 
+import Sidebar from "./SideBar";
 import Header from "./Header";
 import { useState } from "react";
 
@@ -7,29 +9,31 @@ const Layout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="flex h-screen overflow-x-hidden">
-    
+    <div className="h-screen w-full flex overflow-hidden">
+      {/* Sidebar */}
       <div
-        className={`fixed inset-y-0 left-0 z-40 w-64 bg-white shadow-lg transform ${
-          sidebarOpen ? 'translate-x-0' : '-translate-x-full'
-        } transition-transform duration-300 ease-in-out md:relative md:translate-x-0 md:block`}
+        className={`fixed inset-y-0 left-0 z-40 w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out 
+          ${sidebarOpen ? "translate-x-0" : "-translate-x-full"} 
+          md:relative md:translate-x-0 md:block`}
       >
-       
-        <Sidebar onClose={() => setSidebarOpen(false)} /> 
+        <Sidebar onClose={() => setSidebarOpen(false)} />
       </div>
 
-  
+    
       {sidebarOpen && (
         <div
           className="fixed inset-0 bg-black bg-opacity-50 z-30 md:hidden"
           onClick={() => setSidebarOpen(false)}
-        ></div>
+        />
       )}
 
-      <div className={`flex-1 flex flex-col ${sidebarOpen ? '' : ''}`}>
-  
+   
+      <div className="flex-1 flex flex-col overflow-hidden ml-0 ">
+    
         <Header onToggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
-        <main className="flex-1 p-4 overflow-y-auto">
+
+    
+        <main className="flex-1 overflow-y-auto p-4">
           <Outlet />
         </main>
       </div>
